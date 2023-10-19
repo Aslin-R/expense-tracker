0ca726f30a96c1ba8b7bbfd0483ee9a60a8f0bcc
Aslin Joel R <aslinjoelr123@gmail.com>
2023-10-19 08:34:47 +0000

    installed

--- mvp-starter/firebase/firebase.js
+++ mvp-starter/firebase/firebase.js
@@ -14,3 +14,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpmNJnzdN7k7rV72lXQuncEjeGKc8p-v8",
+  authDomain: "expense-tracker-286a1.firebaseapp.com",
+  projectId: "expense-tracker-286a1",
+  storageBucket: "expense-tracker-286a1.appspot.com",
+  messagingSenderId: "880118237063",
+  appId: "1:880118237063:web:334e1dd8d66765de34b522",
+  measurementId: "G-QDWJHJCTKV"
+};
+
+
+const app = initializeApp(firebaseConfig);
